ea8e93b6123e2fc67b6c8b8f1a0a81a576d400cf
johnrhimawan <65337049+johnrhimawan@users.noreply.github.com>
2025-03-15 23:08:19 +0800

    feat: add http response

--- src/server.cpp
+++ src/server.cpp
@@ -8,19 +8,21 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+using namespace std;
+
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << unitbuf;
+  cerr << unitbuf;
   
   // You can use print statements as follows for debugging, they'll be visible when running tests.
-  std::cout << "Logs from your program will appear here!\n";
+  cout << "Logs from your program will appear here!\n";
 
   // Uncomment this block to pass the first stage
   
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
+   cerr << "Failed to create server socket\n";
    return 1;
   }
   
@@ -28,7 +30,7 @@ int main(int argc, char **argv) {
   // // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
+    cerr << "setsockopt failed\n";
     return 1;
   }
   
@@ -38,23 +40,25 @@ int main(int argc, char **argv) {
   server_addr.sin_port = htons(4221);
   
   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 4221\n";
+    cerr << "Failed to bind to port 4221\n";
     return 1;
   }
   
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
+    cerr << "listen failed\n";
     return 1;
   }
   
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
   
-  std::cout << "Waiting for a client to connect...\n";
+  cout << "Waiting for a client to connect...\n";
   
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  std::cout << "Client connected\n";
+  int client = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  string message = "HTTP/1.1 200 OK\r\n\r\n";
+  send(client, message.c_str(), message.size(), 0);
+  cout << "Client connected\n";
   
   close(server_fd);
 
